a56d3bdfc238ffcc351620853cd61f5183a49a7f
Jay-Kakkar <69637792+Jay-Kakkar@users.noreply.github.com>
2020-10-30 22:35:50 +0530

    Update app_src_androidTest_java_com_example_diceroller_ExampleInstrumentedTest.kt

--- app_src_androidTest_java_com_example_diceroller_ExampleInstrumentedTest.kt
+++ app_src_androidTest_java_com_example_diceroller_ExampleInstrumentedTest.kt
@@ -13,6 +13,7 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+//commit 5
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
